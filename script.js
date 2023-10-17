187f5ef6baaf14dc5e649e55d57e0e9d990704b5
Ishaan Mahajan <ishaanamahajan@gmail.com>
2023-10-17 07:56:26 -0500

    projects

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // Smooth scrolling
+function toggleDetails(id) {
+    let details = document.getElementById(id);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
